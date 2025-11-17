f24934ffe4150bb68e32e0babf361395dd6b7c1e
Vasantha62 <125265788+Vasantha62@users.noreply.github.com>
2025-11-17 16:59:28 +0530

    Add files via upload

---
+++ ReadNumberDisplayUnit.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ReadNumberDisplayUnit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number (1, 10, 100, 1000): ");
+        int num = sc.nextInt();
+
+        if (num == 1) {
+            System.out.println("Unit");
+        } 
+        else if (num == 10) {
+            System.out.println("Ten");
+        } 
+        else if (num == 100) {
+            System.out.println("Hundred");
+        } 
+        else if (num == 1000) {
+            System.out.println("Thousand");
+        } 
+        else {
+            System.out.println("Invalid input! Enter only 1, 10, 100, or 1000.");
+        }
+    }
+}
