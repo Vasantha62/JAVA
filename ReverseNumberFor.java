013db31d7285a4df4df6d883314be36de195a294
Vasantha62 <125265788+Vasantha62@users.noreply.github.com>
2025-11-17 17:47:14 +0530

    Add files via upload

---
+++ ReverseNumberFor.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ReverseNumberFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int rev = 0;
+
+        
+        for (; n != 0; n = n / 10) {
+            int digit = n % 10;        
+            rev = rev * 10 + digit;
+        }
+
+        System.out.println("Reversed Number: " + rev);
+    }
+}
