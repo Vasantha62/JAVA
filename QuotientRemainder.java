935fbe11c3d201e7d1045befc30b3fba428ed952
Vasantha62 <125265788+Vasantha62@users.noreply.github.com>
2025-11-20 15:48:35 +0530

    Add files via upload

---
+++ QuotientRemainder.java
@@ -0,0 +1,15 @@
+public class QuotientRemainder {
+    public static void main(String[] args) {
+
+        int dividend = 25;
+        int divisor = 4;
+
+        int quotient = dividend / divisor;   
+        int remainder = dividend % divisor;  
+
+        System.out.println("Dividend: " + dividend);
+        System.out.println("Divisor: " + divisor);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+}
