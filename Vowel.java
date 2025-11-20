4f3f58ccbe71547d29b79315cc3679684ec87a4e
Vasantha62 <125265788+Vasantha62@users.noreply.github.com>
2025-11-20 15:41:02 +0530

    Add files via upload

---
+++ Vowel.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Vowel {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char ch = sc.next().toLowerCase().charAt(0); 
+
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch + " is a Vowel");
+                break;
+
+            default:
+                if (ch >= 'a' && ch <= 'z') {
+                    System.out.println(ch + " is a Consonant");
+                } else {
+                    System.out.println("Not an Alphabet");
+                }
+        }
+    }
+}
+ 
