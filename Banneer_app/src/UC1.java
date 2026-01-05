65752f47c41efbabd1c452ae09510c7216144ca6
Vasantha135 <ragimekalapallivasantha@gmail.com>
2026-01-05 17:04:27 +0530

    FEAT : added UC-1 oops_banner_app

---
+++ Banneer_app/src/UC1.java
@@ -0,0 +1,20 @@
+/**
+ * OOPS Banner Application
+ *
+ * Prints the full form and banner of OOPS
+ * using multiple use cases.
+ *
+ * @author Vasantha
+ * @version 1.0
+**/
+public class UC1 {
+    public static void main(String[] args){
+        System.out.println("USE-CASE-1");
+        System.out.println("----------------------------------------------------------------------------------------");
+        Uc1();
+    }
+    public static void Uc1()
+    {
+        System.out.println("\"OOPS\" Object Oriented Programming System\n");
+    }
+}
